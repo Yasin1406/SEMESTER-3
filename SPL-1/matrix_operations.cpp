7e6dc15ceb8b2d3ad8eb5401d2296c0d705be344
Yasin1406 <121766143+Yasin1406@users.noreply.github.com>
2023-08-09 04:26:00 +0600

    Add files via upload

--- SPL-1/matrix_operations.cpp
+++ SPL-1/matrix_operations.cpp
@@ -1,62 +1,121 @@
 #include<bits/stdc++.h>
 using namespace std;
-int m1,n1,m2,n2;
-vector<vector<double>> mat1;
-vector<vector<double>> mat2;
-vector<vector<double>> mat_mul;
-vector<vector<double>> mat_add;
-vector<vector<double>> mat_sub;
-void addition();
-void subtract();
-void multiplication();
+void addition(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3);
+void subtraction(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3);
+void transpose(vector<vector<double>> &mat1,vector<vector<double>> &mat2);
+void multiplication(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3);
+double determinant(vector<vector<double>> &mat1);
 int main(){
-    printf("Enter the dimension of the 1st matrix: ");
-    scanf("%d %d",&m1,&n1);
-    printf("Enter the dimension of the 1st matrix: ");
-    scanf("%d %d",&m2,&n2);
+    int m1,m2,n1,n2;
+    vector<vector<double>> mat1,mat2,mat3;
+    cout<<"Enter the dimension of the 1st matrix: ";
+    cin>>m1>>n1;
+    // cout<<"Enter the dimension of the 2nd matrix: ";
+    // cin>>m2>>n2;
     int i,j;
     mat1.resize(m1,vector<double>(n1));
-    mat2.resize(m2,vector<double>(n2));
-    if(n1==m2){
-        mat_mul.resize(m1,vector<double>(n2));
-    }
-    if(m1==m2&&n1==n2){
-        mat_add.resize(m1,vector<double>(n1));
-        mat_sub.resize(m2,vector<double>(n2));
-    }
+    //mat2.resize(m2,vector<double>(n2));
+    
+    
     printf("Enter matrix-1:\n");
     for(i=0;i<m1;i++){
         for(j=0;j<n1;j++){
-            scanf("%d",&mat1[i][j]);
+            cin>>mat1[i][j];
         }
     }
-    printf("Enter matrix-2:\n");
-    for(i=0;i<m2;i++){
-        for(j=0;j<n2;j++){
-            scanf("%d",&mat2[i][j]);
-        }
-    }
+    // printf("Enter matrix-2:\n");
+    // for(i=0;i<m2;i++){
+    //     for(j=0;j<n2;j++){
+    //         cin>>mat2[i][j];
+    //     }
+    // }
+
+    // addition(mat1,mat2,mat3);
+    // subtraction(mat1,mat2,mat3);
+    // transpose(mat1,mat2);
+    // cout<<"After subtracition:\n";
+    // for(i=0;i<m1;i++){
+    //     for(j=0;j<n1;j++){
+    //         cout<<mat3[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // multiplication(mat1,mat2,mat3);
+    // cout<<"After multiplication:"<<endl;
+    // for(i=0;i<mat3.size();i++){
+    //     for(j=0;j<mat3[i].size();j++){
+    //         cout<<mat3[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    cout<<"determinant: "<<determinant(mat1)<<endl;
+    return 0;
 
 }
-void addition(){
-    if(!(m1==m2&&n1==n2)){
-        printf("Invalid dimensions for addition\n");
-        return;
-    }
-    for(int i=0;i<m1;i++){
-        for(int j=0;j<n2;j++){
-            mat_add[i][j]=mat1[i][j]+mat2[i][j];
+void addition(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3){
+    mat3.resize(mat1.size(),vector<double>(mat1[0].size()));
+    for(int i=0;i<mat1.size();i++){
+        for(int j=0;j<mat1[i].size();j++){
+            mat3[i][j]=mat1[i][j]+mat2[i][j];
         }
     }
 }
-void subtraction(){
-    if(!(m1==m2&&n1==n2)){
-        printf("Invalid dimensions for subtraction\n");
-        return;
-    }
-    for(int i=0;i<m1;i++){
-        for(int j=0;j<n2;j++){
-            mat_sub[i][j]=mat1[i][j]-mat2[i][j];
+void subtraction(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3){
+    mat3.resize(mat1.size(),vector<double>(mat1[0].size()));
+    for(int i=0;i<mat1.size();i++){
+        for(int j=0;j<mat1[i].size();j++){
+            mat3[i][j]=mat1[i][j]-mat2[i][j];
         }
     }
+}
+void transpose(vector<vector<double>> &mat1,vector<vector<double>> &mat2){
+    mat2.resize(mat1[0].size(),vector<double>(mat1.size()));
+    for(int i=0;i<mat1.size();i++){
+        for(int j=0;j<mat1[i].size();j++){
+            mat2[j][i]=mat1[i][j];
+        }
+    }
+}
+
+void multiplication(vector<vector<double>> &mat1,vector<vector<double>> &mat2,vector<vector<double>> &mat3){
+    if(mat1[0].size()!=mat2.size()){
+        cout<<"Dimension doesn't match for multiplication!"<<endl;
+        return;
+    }
+    mat3.resize(mat1.size(),vector<double> (mat2[0].size()));
+    for(int i=0;i<mat1.size();i++){
+        for(int j=0;j<mat2[0].size();j++){
+            for(int k=0;k<mat2.size();k++){
+                mat3[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+        }
+    }
+}
+
+double determinant(vector<vector<double>> &mat1){
+       double det,prod;
+       int i,j,k,l;
+        for(i=0;i<mat1.size();i++){
+            prod=1;
+            k=i;
+            for(j=0;j<mat1.size();j++){
+                prod*=mat1[j][k%(mat1.size())];
+                k++;
+            }
+            det+=prod;
+        }
+        cout<<endl;
+        for(i=mat1.size()-1;i>=0;i--){
+            prod=1;
+            k=i;
+            for(j=0;j<mat1.size();j++){
+                if(k<0){
+                    k=mat1.size()-1;
+                }
+                prod*=mat1[j][k%(mat1.size())];
+                k--;
+            }
+            det-=prod;
+        }
+        return det;
 }
